f1ccdb2d29307fe2024c090b578ffcdfdf942797
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-07-24 05:50:19 +0200

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let ButtonVal = document.getElementById('enterBtn');
+let statusVal = document.getElementById('status');
+
+function changeStatus() {
+	statusVal.innerHTML = '<h1>Enter Metaverse</h1>';
+}
+
+ButtonVal.addEventListener('click',changeStatus);
